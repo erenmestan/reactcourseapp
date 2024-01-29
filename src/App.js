8940976737a181f8f61a406e41d607e47a2fd361
erenmestan <ermestan@gmail.com>
2024-01-29 16:36:54 +0300

    Added loading screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+import Courses from './Courses';
+import Loading from './Loading';
 
 function App() {
+    const [courses, setCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const fetchCourses = async () => {
+        setLoading(true);
+        try{
+            const response = await axios.get('http://localhost:3004/courses');
+            setCourses(response.data);
+            setLoading(false);
+        }
+        catch (e) {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+        fetchCourses();
+    }, []);
   return (
     <div className="App">
-      Test
+        {loading ? ( <Loading/>) : <Courses courses={courses}/> }
+      <Courses course={courses} />
     </div>
   );
 }
